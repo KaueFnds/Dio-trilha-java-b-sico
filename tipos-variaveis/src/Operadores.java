3b838adb3c2548799d008ea38ff9bc4418154270
Kauê Fernandes <andersonkaue19@gmail.com>
2024-05-21 19:34:53 -0300

    Operadores aula 2

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,12 @@
+public class Operadores {
+    
+    public static void main(String[] args) {
+        int numero = 5;
+
+        numero = -numero; // Operador unario de valor negativo 
+        System.out.println( numero); 
+
+        numero = + numero * -1; // Como torna o numero positivo novamente
+        System.out.println(numero);
+    }
+}
